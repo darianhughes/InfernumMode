38155e8d815dce865a279cba9abc0f73a05fc036
Habble53 <87115317+Habble53@users.noreply.github.com>
2026-08-08 10:03:29 +0300

    Fix Trippy buff crashing on Ceaseless Void fight

---
+++ Core/GlobalInstances/InfernumGlobalBuff.cs
@@ -0,0 +1,18 @@
+﻿using CalamityMod;
+using CalamityMod.Buffs.Alcohol;
+using CalamityMod.NPCs;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace InfernumMode.Core.GlobalInstances
+{
+    public class InfernumGlobalBuff : GlobalBuff
+    {
+        public override void Update(int type, Player player, ref int buffIndex)
+        {
+            // Kill stupid crash
+            if (type == ModContent.BuffType<Trippy>() && CalamityGlobalNPC.voidBoss != -1)
+                player.Calamity().trippy = false;
+        }
+    }
+}
